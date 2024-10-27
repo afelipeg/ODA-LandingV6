f22077c022306bb8ce540ad4f557b449aec92029
ANDRES GUTIERREZ <afelipeg@gmail.com>
2024-10-26 22:42:37 -0600

    Update Video.tsx
    Video Security Enhancements

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -7,6 +7,17 @@ function Video() {
     threshold: 0.1,
   });
 
+  const handleContextMenu = (e) => {
+    e.preventDefault();
+  };
+
+  React.useEffect(() => {
+    document.addEventListener('contextmenu', handleContextMenu);
+    return () => {
+      document.removeEventListener('contextmenu', handleContextMenu);
+    };
+  }, []);
+
   return (
     <section className="py-20 px-4 sm:px-6 lg:px-8 bg-black">
       <div 
@@ -16,9 +27,12 @@ function Video() {
         }`}
       >
         <div className="rounded-2xl overflow-hidden shadow-2xl">
-          <div style={{position:'relative', width:'100%', height:'0px', paddingBottom:'56.250%'}}>
+          <div 
+            style={{position:'relative', width:'100%', height:'0px', paddingBottom:'56.250%'}}
+            onContextMenu={(e) => e.preventDefault()} // Disable right-click on the video container
+          >
             <iframe 
-              allow="fullscreen" 
+              allow="fullscreen"
               allowFullScreen 
               height="100%" 
               src="https://shorturl.at/gikqI?" 
@@ -32,6 +46,7 @@ function Video() {
                 top:'0px', 
                 overflow:'hidden'
               }}
+              onContextMenu={(e) => e.preventDefault()} // Disable right-click on the iframe
             />
           </div>
         </div>
